99fe5062822ccc2203cdd8a360236e545ba85601
Younguk Kim <bluekyu.dev@gmail.com>
2016-12-13 21:06:23 +0900

    Add interval part.

--- 03-Lights/src/main.cpp
+++ 03-Lights/src/main.cpp
@@ -28,6 +28,9 @@
 #include <load_prc_file.h>
 
 #include <render_pipeline/rppanda/showbase/showbase.h>
+#include <render_pipeline/rppanda/interval/lerp_interval.h>
+#include <render_pipeline/rppanda/interval/meta_interval.h>
+
 #include <render_pipeline/rpcore/render_pipeline.h>
 #include <render_pipeline/rpcore/mount_manager.h>
 #include <render_pipeline/rpcore/pluginbase/day_manager.h>
@@ -56,7 +59,7 @@ int main(int argc, char* argv[])
     // ------ End of render pipeline code, thats it! ------
 
 	// Set time of day
-	render_pipeline->get_daytime_mgr()->set_time("4:50");
+	render_pipeline->get_daytime_mgr()->set_time("5:20");
 
     float half_energy = 5000;
     float lamp_fov = 70;
@@ -66,7 +69,79 @@ int main(int argc, char* argv[])
 	NodePath model = window->load_model(window->get_render(), "../share/render_pipeline/models/03-Lights/Scene.bam");
 
 	// Animate balls, this is for testing the motion blur
-	const std::string blend_type = "noBlend";
+	CLerpInterval::BlendType blend_type = CLerpInterval::BT_no_blend;
+
+	{
+		NodePath np = model.find("**/MBRotate");
+
+		rppanda::LerpHprInterval::Parameters params;
+		params.nodepath = np;
+		params.duration = 1.5;
+		params.hpr = LVecBase3(360, 360, 0);
+		params.start_hpr = LVecBase3(0, 0, 0);
+		params.blend_type = blend_type;
+		PT(rppanda::LerpHprInterval) lerp = new rppanda::LerpHprInterval(params);
+		lerp->loop();
+	}
+
+	{
+		NodePath np = model.find("**/MBUpDown");
+		const LVecBase3& np_pos = np.get_pos() - LVecBase3(0, 0, 2);
+
+		rppanda::LerpPosInterval::Parameters params;
+		params.nodepath = np;
+		params.duration = 0.15;
+		params.pos = np_pos + LVecBase3(0, 0, 6);
+		params.start_pos = np_pos;
+		params.blend_type = blend_type;
+		PT(rppanda::LerpPosInterval) lerp1 = new rppanda::LerpPosInterval(params);
+
+		params.pos = np_pos;
+		params.start_pos = np_pos + LVecBase3(0, 0, 6);
+		PT(rppanda::LerpPosInterval) lerp2 = new rppanda::LerpPosInterval(params);
+
+		PT(rppanda::Sequence) seq = new rppanda::Sequence({lerp1, lerp2});
+		seq->loop();
+	}
+
+	{
+		NodePath np = model.find("**/MBFrontBack");
+		const LVecBase3& np_pos = np.get_pos() - LVecBase3(0, 0, 2);
+
+		rppanda::LerpPosInterval::Parameters params;
+		params.nodepath = np;
+		params.duration = 0.15;
+		params.pos = np_pos + LVecBase3(0, 6, 0);
+		params.start_pos = np_pos;
+		params.blend_type = blend_type;
+		PT(rppanda::LerpPosInterval) lerp1 = new rppanda::LerpPosInterval(params);
+
+		params.pos = np_pos;
+		params.start_pos = np_pos + LVecBase3(0, 6, 0);
+		PT(rppanda::LerpPosInterval) lerp2 = new rppanda::LerpPosInterval(params);
+
+		PT(rppanda::Sequence) seq = new rppanda::Sequence({lerp1, lerp2});
+		seq->loop();
+	}
+
+	{
+		NodePath np = model.find("**/MBScale");
+
+		rppanda::LerpScaleInterval::Parameters params;
+		params.nodepath = np;
+		params.duration = 0.15;
+		params.scale = LVecBase3(1.5);
+		params.start_scale = LVecBase3(1);
+		params.blend_type = blend_type;
+		PT(rppanda::LerpScaleInterval) lerp1 = new rppanda::LerpScaleInterval(params);
+
+		params.scale = LVecBase3(1);
+		params.start_scale = LVecBase3(1.5);
+		PT(rppanda::LerpScaleInterval) lerp2 = new rppanda::LerpScaleInterval(params);
+
+		PT(rppanda::Sequence) seq = new rppanda::Sequence({lerp1, lerp2});
+		seq->loop();
+	}
 
     // Generate temperature lamps
     // This shows how to procedurally create lamps.In this case, we
@@ -76,9 +151,9 @@ int main(int argc, char* argv[])
 	render_pipeline->prepare_scene(rpcore::Globals::render);
 
     rpcore::MovementController* controller =  new rpcore::MovementController(rpcore::Globals::base);
-	controller->set_initial_position_hpr(
-		LVecBase3f(-17.2912578583, -13.290019989, 6.88211250305),
-		LVecBase3f(-39.7285499573, -14.6770210266, 0.0));
+	controller->set_initial_position(
+		LVecBase3f(23.9, 42.5, 13.4),
+		LVecBase3f(23.8, 33.4, 10.8));
 	controller->setup();
 
 	framework.main_loop();
