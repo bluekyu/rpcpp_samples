d9e85b334a1227a89baa0e06c79b357ee6f8c12f
bluekyu <bluekyu.dev@gmail.com>
2017-04-27 10:06:42 +0900

    Update cmake files.
    - Apply target based configuration.
    - Remove FindRenderPipeline.

--- cmake/FindPackages.cmake
+++ cmake/FindPackages.cmake
@@ -11,26 +11,11 @@ set(BOOST_INCLUDEDIR "" CACHE PATH "Hint for finding boost include directory")
 set(BOOST_LIBRARYDIR "" CACHE PATH "Hint for finding boost library directory")
 
 find_package(Boost 1.62.0 REQUIRED)
-if(Boost_FOUND)
-    message(STATUS "Boost include path: ${Boost_INCLUDE_DIRS}")
-    if(NOT Boost_USE_STATIC_LIBS)
-        set(Boost_LIBRARIES Boost::dynamic_linking ${Boost_LIBRARIES})
-    endif()
-endif()
 
 # find panda3d
 set(PANDA3D_ROOT "" CACHE PATH "Hint for finding panda3d root directory")
 
 find_package(Panda3d REQUIRED COMPONENTS libp3framework libpanda libpandaexpress libp3dtool libp3dtoolconfig libp3direct libp3interrogatedb)
-if(Panda3d_FOUND)
-    message(STATUS "Panda3D include path: ${Panda3d_INCLUDE_DIRS}")
-endif()
 
 # find render_pipeline
-set(RenderPipeline_ROOT "" CACHE PATH "Hint for finding render pipeline root directory")
-set(RenderPipeline_INCLUDEDIR "" CACHE PATH "Hint for finding render pipeline include directory")
-set(RenderPipeline_LIBRARYDIR "" CACHE PATH "Hint for finding render pipeline library directory")
-find_package(RenderPipeline REQUIRED)
-if(RenderPipeline_FOUND)
-    message(STATUS "render pipeline include path: ${RenderPipeline_INCLUDE_DIRS}")
-endif()
+find_package(render_pipeline REQUIRED)
