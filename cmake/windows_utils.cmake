da3cbc2a8163a18295650200241f3ac84e5f095d
Younguk Kim <bluekyu.dev@gmail.com>
2017-09-14 15:09:53 +0900

    Update windows_util.cmake
    - Add postfix "windows" to add_longpath_manifest
    - Add windows_add_delay_load function

--- cmake/windows_utils.cmake
+++ cmake/windows_utils.cmake
@@ -3,13 +3,16 @@
 
 cmake_minimum_required(VERSION 3.4)
 
-# add_longpath_manifest function.
+# windows_add_longpath_manifest function.
 #
 # This function genereates "longpath.manifest" file if it does not exist.
 # And then, it adds the manifest file to given target as source.
 # The manifest file has "longPathAware" tag for Win32 application.
 # see https://msdn.microsoft.com/en-us/library/windows/desktop/aa365247(v=vs.85).aspx
-function(add_longpath_manifest target_)
+#
+# windows_add_longpath_manifest(<target>)
+# @param    target      Target variable
+function(windows_add_longpath_manifest target_)
     set(manifest_file_path "${CMAKE_BINARY_DIR}/src/windows/longpath.manifest")
 
     if(NOT EXISTS ${manifest_file_path})
@@ -26,4 +29,26 @@ function(add_longpath_manifest target_)
     endif()
 
     target_sources(${target_} PRIVATE ${manifest_file_path})
+endfunction()
+
+# windows_add_delay_load function.
+#
+# This function add "/DELAYLOAD" option to link flags.
+#
+# windows_add_delay_load(<target> <dll_names> [<config>])
+# @param    target      Target variable
+# @param    dll_names   The list of name of dlls.
+# @param    config      Configuration. ex) Release, Debug
+function(windows_add_delay_load target_ dll_names_)
+    if(ARGV2)
+        string(TOUPPER ${ARGV2} configuration)
+        set(configuration "_${configuration}")
+    endif()
+
+    set(flag "")
+    foreach(dll_name ${dll_names_})
+        set(flag "${flag} /DELAYLOAD:${dll_name}")
+    endforeach()
+
+    set_property(TARGET ${target_} APPEND_STRING PROPERTY LINK_FLAGS${configuration} "${flag} ")
 endfunction()
